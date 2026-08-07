a396bfec86251cffabfb8d245e522869749b6ddb
ShadowOfficial <leightonsaxty@gmail.com>
2026-08-07 18:43:06 +0100

    Add overlay renderer scaffolding for flight controller face texture

--- src/main/java/com/flightcomputer/client/render/FlightControllerRenderer.java
+++ src/main/java/com/flightcomputer/client/render/FlightControllerRenderer.java
@@ -1,11 +1,20 @@
 package com.flightcomputer.client.render;
 
 import com.flightcomputer.block.FlightControllerBlockEntity;
+import net.minecraft.resources.ResourceLocation;
 import software.bernie.geckolib.renderer.GeoBlockRenderer;
 
 public class FlightControllerRenderer extends GeoBlockRenderer<FlightControllerBlockEntity> {
 
+    private static final ResourceLocation OVERLAY_TEXTURE = ResourceLocation.fromNamespaceAndPath(
+            "flightcomputer", "textures/block/flight_controller_overlay.png");
+
     public FlightControllerRenderer() {
         super(new FlightControllerModel());
+    }
+
+    @Override
+    public ResourceLocation getTextureLocation(FlightControllerBlockEntity animatable) {
+        return OVERLAY_TEXTURE;
     }
 }
