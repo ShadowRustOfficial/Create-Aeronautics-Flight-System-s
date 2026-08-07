d3c4382ed67a083a9963ba3858deb159c1314152
ShadowOfficial <leightonsaxty@gmail.com>
2026-08-08 00:03:28 +0100

    Add cooling upgrade item type

---
+++ src/main/java/com/flightcomputer/item/CoolingUpgradeItem.java
@@ -0,0 +1,19 @@
+package com.flightcomputer.item;
+
+import net.minecraft.world.item.Item;
+
+/** Marker item for Flight Controller cooling upgrade tiers. */
+public class CoolingUpgradeItem extends Item {
+    public enum Tier { BASIC, IMPROVED, ADVANCED }
+
+    private final Tier tier;
+
+    public CoolingUpgradeItem(Properties properties, Tier tier) {
+        super(properties);
+        this.tier = tier;
+    }
+
+    public Tier tier() {
+        return tier;
+    }
+}
