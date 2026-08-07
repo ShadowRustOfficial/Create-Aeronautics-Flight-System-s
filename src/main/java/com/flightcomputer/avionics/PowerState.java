4c28ba5762708c9ca97f7214b4d07af4e98ad04c
ShadowOfficial <leightonsaxty@gmail.com>
2026-08-08 00:03:13 +0100

    Add controller power state model

---
+++ src/main/java/com/flightcomputer/avionics/PowerState.java
@@ -0,0 +1,10 @@
+package com.flightcomputer.avionics;
+
+/** Discrete power condition for one Flight Controller block entity. */
+public enum PowerState {
+    NORMAL,
+    MEDIUM,
+    LOW,
+    CRITICAL,
+    NO_POWER
+}
