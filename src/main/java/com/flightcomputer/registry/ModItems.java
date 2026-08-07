f0d1f4e1370216aea335ffbbbe5c58f3dfa0934d
ShadowOfficial <leightonsaxty@gmail.com>
2026-08-08 00:03:36 +0100

    Register controller cooling upgrades

--- src/main/java/com/flightcomputer/registry/ModItems.java
+++ src/main/java/com/flightcomputer/registry/ModItems.java
@@ -1,6 +1,7 @@
 package com.flightcomputer.registry;
 
 import com.flightcomputer.FlightComputer;
+import com.flightcomputer.item.CoolingUpgradeItem;
 import com.flightcomputer.item.FlightLinkToolItem;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
@@ -8,18 +9,16 @@ import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 public final class ModItems {
+    public static final DeferredRegister.Items REGISTRY = DeferredRegister.createItems(FlightComputer.MOD_ID);
 
-    public static final DeferredRegister.Items REGISTRY =
-            DeferredRegister.createItems(FlightComputer.MOD_ID);
+    public static final DeferredHolder<Item, BlockItem> FLIGHT_CONTROLLER = REGISTRY.register("flight_controller", () -> new BlockItem(
+            ModBlocks.FLIGHT_CONTROLLER.get(), new Item.Properties()));
 
-    public static final DeferredHolder<Item, BlockItem> FLIGHT_CONTROLLER =
-            REGISTRY.register("flight_controller", () -> new BlockItem(
-                    ModBlocks.FLIGHT_CONTROLLER.get(),
-                    new Item.Properties()
-            ));
+    public static final DeferredHolder<Item, FlightLinkToolItem> FLIGHT_LINK_TOOL = REGISTRY.register("flight_link_tool", () -> new FlightLinkToolItem(new Item.Properties().stacksTo(1)));
 
-    public static final DeferredHolder<Item, FlightLinkToolItem> FLIGHT_LINK_TOOL =
-            REGISTRY.register("flight_link_tool", () -> new FlightLinkToolItem(new Item.Properties().stacksTo(1)));
+    public static final DeferredHolder<Item, CoolingUpgradeItem> BASIC_COOLING = REGISTRY.register("basic_cooling", () -> new CoolingUpgradeItem(new Item.Properties().stacksTo(1), CoolingUpgradeItem.Tier.BASIC));
+    public static final DeferredHolder<Item, CoolingUpgradeItem> IMPROVED_COOLING = REGISTRY.register("improved_cooling", () -> new CoolingUpgradeItem(new Item.Properties().stacksTo(1), CoolingUpgradeItem.Tier.IMPROVED));
+    public static final DeferredHolder<Item, CoolingUpgradeItem> ADVANCED_COOLING = REGISTRY.register("advanced_cooling", () -> new CoolingUpgradeItem(new Item.Properties().stacksTo(1), CoolingUpgradeItem.Tier.ADVANCED));
 
     private ModItems() {}
 }
