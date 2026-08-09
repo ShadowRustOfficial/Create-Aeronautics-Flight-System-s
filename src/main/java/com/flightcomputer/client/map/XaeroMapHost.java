d3694d70e603f61d259c1a62ba7b8b1eb41cca99
ShadowOfficial <leightonsaxty@gmail.com>
2026-08-09 02:49:19 +0100

    Clean unused import after Xaero crash fix

--- src/main/java/com/flightcomputer/client/map/XaeroMapHost.java
+++ src/main/java/com/flightcomputer/client/map/XaeroMapHost.java
@@ -4,7 +4,6 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import xaero.map.WorldMap;
 
