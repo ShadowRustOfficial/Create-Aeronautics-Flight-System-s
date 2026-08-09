c0f89095395f36d7aa62d1eb40404612ba6cf63e
ShadowOfficial <leightonsaxty@gmail.com>
2026-08-09 04:43:56 +0100

    Remove dedicated Waystones marker category

--- src/main/java/com/flightcomputer/map/MarkerCategory.java
+++ src/main/java/com/flightcomputer/map/MarkerCategory.java
@@ -3,7 +3,6 @@ package com.flightcomputer.map;
 public enum MarkerCategory {
     FLIGHT_WAYPOINT("Flight Waypoints", 0x55AAFF),
     XAERO_WAYPOINT("Xaero Waypoints", 0x55AAFF),
-    WAYSTONE("Waystones", 0x55FF55),
     CLAIMED_SUBLEVEL("Claimed Sub-Levels", 0xFFAA00),
     LANDING_PAD("Landing Pads", 0x55FF55);
 
