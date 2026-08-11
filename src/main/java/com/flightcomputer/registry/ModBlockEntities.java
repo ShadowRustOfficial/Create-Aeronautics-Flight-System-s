2c9c0f4dee2fa851ae50d669c8eef44ca9da0671
ShadowOfficial <leightonsaxty@gmail.com>
2026-08-11 06:02:26 +0100

    Register debug generator block entity

--- src/main/java/com/flightcomputer/registry/ModBlockEntities.java
+++ src/main/java/com/flightcomputer/registry/ModBlockEntities.java
@@ -1,6 +1,7 @@
 package com.flightcomputer.registry;
 
 import com.flightcomputer.FlightComputer;
+import com.flightcomputer.block.DebugPowerGeneratorBlockEntity;
 import com.flightcomputer.block.FlightControllerBlockEntity;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.level.block.entity.BlockEntityType;
@@ -8,15 +9,16 @@ import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 public final class ModBlockEntities {
-
     public static final DeferredRegister<BlockEntityType<?>> REGISTRY =
             DeferredRegister.create(Registries.BLOCK_ENTITY_TYPE, FlightComputer.MOD_ID);
 
     public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<FlightControllerBlockEntity>> FLIGHT_CONTROLLER =
             REGISTRY.register("flight_controller", () -> BlockEntityType.Builder.of(
-                    FlightControllerBlockEntity::new,
-                    ModBlocks.FLIGHT_CONTROLLER.get()
-            ).build(null));
+                    FlightControllerBlockEntity::new, ModBlocks.FLIGHT_CONTROLLER.get()).build(null));
+
+    public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<DebugPowerGeneratorBlockEntity>> DEBUG_POWER_GENERATOR =
+            REGISTRY.register("debug_power_generator", () -> BlockEntityType.Builder.of(
+                    DebugPowerGeneratorBlockEntity::new, ModBlocks.DEBUG_POWER_GENERATOR.get()).build(null));
 
     private ModBlockEntities() {}
 }
