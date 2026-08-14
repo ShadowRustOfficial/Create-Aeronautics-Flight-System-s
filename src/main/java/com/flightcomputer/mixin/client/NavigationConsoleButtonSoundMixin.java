f778f78a9f78f47549d2375191b8da9960374ded
ShadowOfficial <leightonsaxty@gmail.com>
2026-08-14 04:43:16 +0100

    Hook shared widget sound method safely

--- src/main/java/com/flightcomputer/mixin/client/NavigationConsoleButtonSoundMixin.java
+++ src/main/java/com/flightcomputer/mixin/client/NavigationConsoleButtonSoundMixin.java
@@ -1,6 +1,7 @@
 package com.flightcomputer.mixin.client;
 
 import com.flightcomputer.client.AudioUiSoundBridge;
+import net.minecraft.client.gui.components.AbstractWidget;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.sounds.SoundManager;
 import org.spongepowered.asm.mixin.Mixin;
@@ -8,11 +9,11 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Button.class)
+@Mixin(AbstractWidget.class)
 public abstract class NavigationConsoleButtonSoundMixin {
     @Inject(method = "playDownSound", at = @At("HEAD"), cancellable = true)
     private void flightcomputer$replaceSound(SoundManager soundManager, CallbackInfo ci) {
-        Button button = (Button) (Object) this;
+        if (!(this instanceof Button button)) return;
         if (net.minecraft.client.Minecraft.getInstance().screen instanceof com.flightcomputer.client.gui.NavigationConsoleScreen) {
             AudioUiSoundBridge.playForButton(button);
             ci.cancel();
