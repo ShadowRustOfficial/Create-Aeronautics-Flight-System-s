f82790f4c0a77b65bc6573708a23e4da3772827f
ShadowOfficial <leightonsaxty@gmail.com>
2026-08-14 04:57:24 +0100

    Fix button sound mixin compile error

--- src/main/java/com/flightcomputer/mixin/client/NavigationConsoleButtonSoundMixin.java
+++ src/main/java/com/flightcomputer/mixin/client/NavigationConsoleButtonSoundMixin.java
@@ -1,7 +1,6 @@
 package com.flightcomputer.mixin.client;
 
 import com.flightcomputer.client.AudioUiSoundBridge;
-import net.minecraft.client.gui.components.AbstractWidget;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.sounds.SoundManager;
 import org.spongepowered.asm.mixin.Mixin;
@@ -9,11 +8,11 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(AbstractWidget.class)
+@Mixin(Button.class)
 public abstract class NavigationConsoleButtonSoundMixin {
     @Inject(method = "playDownSound", at = @At("HEAD"), cancellable = true)
     private void flightcomputer$replaceSound(SoundManager soundManager, CallbackInfo ci) {
-        if (!(this instanceof Button button)) return;
+        Button button = (Button) (Object) this;
         if (net.minecraft.client.Minecraft.getInstance().screen instanceof com.flightcomputer.client.gui.NavigationConsoleScreen) {
             AudioUiSoundBridge.playForButton(button);
             ci.cancel();
