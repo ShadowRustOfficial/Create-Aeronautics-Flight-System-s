dacb873a13771d5fd359da891698c04246fed183
ShadowOfficial <leightonsaxty@gmail.com>
2026-08-17 14:19:15 +0100

    Register powered controller contact payload on mod bus

--- src/main/java/com/flightcomputer/FlightComputer.java
+++ src/main/java/com/flightcomputer/FlightComputer.java
@@ -1,6 +1,7 @@
 package com.flightcomputer;
 
 import com.flightcomputer.network.FlightComputerUiSoundNetwork;
+import com.flightcomputer.network.FlightControllerContactNetwork;
 import com.flightcomputer.registry.ModBlockEntities;
 import com.flightcomputer.registry.ModBlocks;
 import com.flightcomputer.registry.ModItems;
@@ -27,6 +28,7 @@ public final class FlightComputer {
         // RegisterPayloadHandlersEvent is a mod-bus event. Register directly instead of using
         // the deprecated EventBusSubscriber.bus() API.
         modBus.addListener(FlightComputerUiSoundNetwork::register);
+        modBus.addListener(FlightControllerContactNetwork::register);
 
         modContainer.registerConfig(ModConfig.Type.COMMON, FlightComputerConfig.SPEC);
         modBus.addListener(this::commonSetup);
